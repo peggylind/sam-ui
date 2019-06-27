046e76e1209512c74c3db37a7700714b750e76ad
Peggy Lindner <plindner@uh.edu>
2019-06-27 11:15:59 -0500

    - updates for sparklyr sample

--- r-scripts/spark_filter.R
+++ r-scripts/spark_filter.R
@@ -1,10 +1,8 @@
 library(sparklyr)
-library(tidyverse)
-library(dplyr)
 
 
-setwd("~/Development/humana/sampleRDS")
-spark_dir = "/data/spark/"
+#setwd("~/Development/humana/sampleRDS")
+#spark_dir = "/data/spark/"
 sc = spark_connect(master = "local")
 
 med = spark_read_csv(sc, name = "med", path = "med_tab_10k.csv")
@@ -18,7 +16,7 @@ counts_by_code
 (toc = Sys.time() - tic)
 
 hpylori <- med %>% filter_at(vars(contains("DIAGNOSIS_CODE")), any_vars(. == 04186))
-hpyloriSyst
+hpylori
 
 
 
