50aeeb0f212b22d2e82ccd9d852e8b3c32fa4c7d
Peggy Lindner <peggylind@gmail.com>
2019-07-08 15:30:04 -0500

    - working on sparkr

--- r-scripts/spark_filter.R
+++ r-scripts/spark_filter.R
@@ -8,7 +8,7 @@ spark_dir = "/data/spark/"
 sc = spark_connect(master = "local")
 
 med = spark_read_csv(sc, name = "med", path = "med_tab_10k.csv")
-med <- read_csv("med_tab_10k.csv")
+med <- read_table("med_tab_10k.csv",)
 
 tic = Sys.time()
 counts_by_code <- med %>%
@@ -17,8 +17,9 @@ counts_by_code <- med %>%
 counts_by_code
 (toc = Sys.time() - tic)
 
-hpylori <- med %>% filter_at(vars(contains("DIAGNOSIS_CODE")), any_vars(. == 04186))
-hpyloriSyst
+eclam <- med_tab_10k%>% filter_at(vars(contains("DIAGNOSIS_CODE")), any_vars(. %in% c('6424', '64240','64241','64242','64243','64244','6425','64250','64251','64252','64253','64254','6426','64260','64261','64262','64263','64264','6427','64270','64271','64272','64273','64274')))
+eclam
+
 
 
 
